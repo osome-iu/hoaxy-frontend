c005ce7edf5b8d695f301653aac1ab52b98c4a10
Shao <rojeeer@gmail.com>
2017-08-14 18:45:20 -0400

    Update config.example.js
    When consuming Mashape API, `X-Mashap-Proxy-Secret` is not needed.

--- config.example.js
+++ config.example.js
@@ -2,26 +2,22 @@ configuration = function(){
 	var obj = {}
 
 	var mashape_key = "YOURKEY";
-	var proxy_secret = "YOURSECRET";
 
 	obj.articles_url = "https://api-hoaxy.p.mashape.com/articles";
 	obj.articles_headers = {
 		"X-Mashape-Key": mashape_key,
-		"X-Mashape-Proxy-Secret": proxy_secret,
 		"Accept": "application/json"
 	};
 
 	obj.timeline_url =  "https://api-hoaxy.p.mashape.com/timeline";
 	obj.timeline_headers = {
 		"X-Mashape-Key": mashape_key,
-		"X-Mashape-Proxy-Secret": proxy_secret,
 		"Accept": "application/json"
 	};
 
 	obj.network_url = "https://api-hoaxy.p.mashape.com/network";
 	obj.network_headers = {
 		"X-Mashape-Key": mashape_key,
-		"X-Mashape-Proxy-Secret": proxy_secret,
 		"Accept": "application/json"
 	};
 
