b11059fb22b2e62c65d268cd8ee17f38ca8f101c
yangkcatiu <yangkc@iu.edu>
2017-08-28 15:50:18 -0400

    First commit for the Hoaxy-Botometer API with following features: 1. Database model for the model 2. A GET method that can return user's information as json according to their user_id

---
+++ backend/API/launcher.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+from dbmodels import api, Botbase
+#from flask.ext.sqlalchemy import SQLAlchemy
+
+@api.route("/")
+def hello():
+    return "Hello World!"
+
+@api.route("/userids/<int:user_id>", methods=["GET"])
+def findUser(user_id):
+    query_resutls = Botbase.query.filter(Botbase.user_id==user_id).all()
+    if query_resutls:
+        return jsonify(query_resutls[0].getJsonRepr())
+    else:
+        return jsonify({})
+
+if __name__ == "__main__":
+    #pass
+    api.run()
+    #bot = Botbase.query.filter(Botbase.user_id==12345).all()
+    #print type(getUser(12345).user_id)
+    #print getUser(12345).user_id
