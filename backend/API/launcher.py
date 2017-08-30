4d9e188eaf7fd016426d2a4d85040eba5489eafc
yangkcatiu <yangkc@iu.edu>
2017-08-30 17:59:06 -0400

    add the skeleton for the flowcharts

--- backend/API/launcher.py
+++ backend/API/launcher.py
@@ -1,14 +1,79 @@
 # -*- coding: utf8 -*-
 from dbmodels import api, BotbaseModel
 from flask import jsonify, request
+import ConfigParser
+from datetime import datetime
 
 
 def queryStringParser(query_string):
     """
-    query_string : the query string with the format "userid1,userid2,userid3"
-    return       : a list of user ids
+    query_string   str: the query string with the format "userid1,userid2,userid3"
+    return        list: a list of user ids
     """
     return map(int, query_string.split(","))
+
+
+def getUserScore(user_id):
+    """
+    user_id    int: user id
+    return    json: the user's scores
+              None: if fail to get the user's scores
+    """
+    # load the configurations
+    Config = ConfigParser.ConfigParser()
+    Config.read("./config.cfg")
+    age_min = int(Config.get("FlowChart", "age_min"))
+    age_max = int(Config.get("FlowChart", "age_max"))
+    reqs_max = int(Config.get("FlowChart", "reqs_max"))
+    # BotbaseModel does not support indexing?
+    user_entries = BotbaseModel.query.filter(BotbaseModel.user_id == user_id).order_by(BotbaseModel.time_stamp).all()
+    if user_entries:
+        # see how the results are ordered to decide 0 or -1
+        user_latest_entry = user_entries[0]
+        timedelta_age = datetime.now() - datetime(user_latest_entry.time_stamp)
+        age = timedelta_age.total_seconds()
+
+        if age < age_min:
+            return  user_latest_entry.all_bot_scores
+        elif age > age_max:
+            return computeNewScore(user_id)
+        else:
+            expected_tweets = age * user_latest_entry.tweets_per_day / 86400.
+            if(expected_tweets > 100 or user_latest_entry.num_requests > reqs_max):
+                return computeNewScore(user_id)
+            else:
+                return user_latest_entry.all_bot_scores
+    else:
+        return computeNewScore(user_id)
+
+
+def queryBotometer(user_id):
+    """
+    user_id    int: user id
+    return    json: scores results from Botometer
+    """
+    return None
+
+
+def insertUserEntry(user_entry):
+    """
+    user_entry  dict: a dict contains the user's information
+    return      True: if successfully insert the entry
+               False: if fail to insert the entry
+    """
+    # need to update the tweets_per_day
+    return False
+
+
+def computeNewScore(user_id):
+    """
+    user_id    int: user id
+    return    json: scores results from Botometer
+    """
+    result_from_botometer = queryBotometer(user_id)
+    if result_from_botometer:
+        insertUserEntry(result_from_botometer)
+    return result_from_botometer
 
 
 @api.route("/")
@@ -19,12 +84,22 @@ def hello():
 
 @api.route("/api/scores", methods=["GET"])
 def getScores():
+    """
+    The scorese retrival endpoint.
+    Parse the query string, get user scores according to user_ids then return as json.
+    """
+
+    # parse the query string
     user_id_list = queryStringParser(request.args["query"])
+
+    # initialize the dict to store the scores
     user_scores = dict()
+
+    # try to get all the scores
     for user_id in user_id_list:
-        user_info = BotbaseModel.query.filter(BotbaseModel.user_id == user_id).all()
-        if user_info:
-            user_scores[user_id] = user_info[0].all_bot_scores
+        user_score = getUserScore(user_id)
+        if user_score:
+            user_scores[user_id] = user_score
     return jsonify(user_scores)
 
 
