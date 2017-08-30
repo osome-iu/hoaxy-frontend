3163af5c486f4285f24357080af4f56838f36fae
yangkcatiu <yangkc@iu.edu>
2017-08-30 15:44:28 -0400

    Respond to queries in format of "/scores?query=userid1,userid2,userid3", then return a json with all the required users scores

--- backend/API/launcher.py
+++ backend/API/launcher.py
@@ -1,23 +1,32 @@
 # -*- coding: utf8 -*-
-from dbmodels import api, Botbase
-from flask import jsonify
-#from flask.ext.sqlalchemy import SQLAlchemy
+from dbmodels import api, BotbaseModel
+from flask import jsonify, request
+
+
+def queryStringParser(query_string):
+    """
+    query_string : the query string with the format "userid1,userid2,userid3"
+    return       : a list of user ids
+    """
+    return map(int, query_string.split(","))
+
 
 @api.route("/")
 def hello():
-    return "Hello World!"
+    return """Welcome to the Hoaxy-Botometer API.\nGet user scores with '/api/
+                scores?query=userid1,userid2,userid3'."""
 
-@api.route("/userids/<int:user_id>", methods=["GET"])
-def findUser(user_id):
-    query_resutls = Botbase.query.filter(Botbase.user_id==user_id).all()
-    if query_resutls:
-        return jsonify(query_resutls[0].getJsonRepr())
-    else:
-        return jsonify({})
+
+@api.route("/api/scores", methods=["GET"])
+def getScores():
+    user_id_list = queryStringParser(request.args["query"])
+    user_scores = dict()
+    for user_id in user_id_list:
+        user_info = BotbaseModel.query.filter(BotbaseModel.user_id == user_id).all()
+        if user_info:
+            user_scores[user_id] = user_info[0].all_bot_scores
+    return jsonify(user_scores)
+
 
 if __name__ == "__main__":
-    #pass
-    api.run()
-    #bot = Botbase.query.filter(Botbase.user_id==12345).all()
-    #print type(getUser(12345).user_id)
-    #print getUser(12345).user_id
+    api.run(debug=True)
