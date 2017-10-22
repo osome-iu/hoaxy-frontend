1d26314c9e951c3dedcb39c51b959328f44330b6
yangkcatiu <yangkc@iu.edu>
2017-10-22 16:29:34 -0400

    little modification

--- backend/API/benchmark.py
+++ backend/API/benchmark.py
@@ -9,7 +9,7 @@ def dbQueryUserScreenNameIn(user_names):
     result = botscore_connection.execute(
         sqlalchemy.text(
             """
-            SELECT DISTINCT ON (screen_name) id, user_id, screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_submitted_timeline_tweets, num_requests
+            SELECT DISTINCT ON (screen_name) id, user_id, screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_requests
             from botscore
             where screen_name IN :screen_names
             ORDER BY screen_name, time_stamp DESC
@@ -27,11 +27,11 @@ def dbQueryUserScreenName(user_names):
         sqlalchemy.text(
             """
             WITH temptable AS (
-                SELECT id, user_id, names.screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_submitted_timeline_tweets, num_requests
+                SELECT id, user_id, names.screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_requests
                 FROM botscore
                 JOIN UNNEST(:screen_names) AS names(screen_name) ON botscore.screen_name = names.screen_name
             )
-            SELECT id, user_id, screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_submitted_timeline_tweets, num_requests
+            SELECT id, user_id, screen_name, all_bot_scores, bot_score_english, bot_score_universal, time_stamp, tweets_per_day, num_requests
             from temptable
             JOIN (
                 SELECT temptable.screen_name AS latest_user_screen_name, max(time_stamp) AS latesttimestamp
@@ -65,8 +65,12 @@ if __name__ == "__main__":
     #name_list = prepareNameList(10)
     #dbQueryUserScreenName(name_list)
     #print(prepareNameList(10))
-    name_list = prepareNameList(10)
+    #name_list = prepareNameList(10)
+    name_list = ["Fontane_opr", "kwautuh_peace", "claireclose"]
     resultIn = dbQueryUserScreenNameIn(name_list)
     result = dbQueryUserScreenName(name_list)
-    print(len(list(result)))
-    print(len(list(resultIn)))
+    for i in result:
+        print(i)
+
+    for i in resultIn:
+        print(i)
