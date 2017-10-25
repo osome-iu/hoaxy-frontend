c8420c977b99476a487b090cda79710a885d2422
Mihai Avram <mihai_avram2001@yahoo.com>
2017-10-25 15:16:45 -0400

    Completed Cron Job script to fetch user profile, tweets, and mentions using the Twitter API, now what is left to do is to create the CRON job itself that runs this script

--- backend/tools/feedback-table-cron-job/FetchAndUpdateFeedback_ProfilesTimelinesMentions.py
+++ backend/tools/feedback-table-cron-job/FetchAndUpdateFeedback_ProfilesTimelinesMentions.py
@@ -11,6 +11,11 @@ import psycopg2
 import json
 #for error logging
 import sys, traceback
+#Importing Twitter library essentials
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+#for obtaining keys and passwords needed to execute the code using various tools and libraries
+keysandsecrets_json = json.load(open('iuni-twitterapi-keys-and-secrets.json'))
 
 #can be used for debugging if exceptions start to occur
 def print_exception():
@@ -56,12 +61,26 @@ def populate_target_profiles_timelinetweets_mentiontweets(field_scope, populated
             botometer_cursor.execute("""UPDATE public.feedback SET target_mention_tweets = %s WHERE target_user_id = %s;""",\
                                    (json.dumps(profile), target_user_id))
 
-
         #commiting changes
         pgsqlconn.commit()
 
 #MAIN CODE
 if __name__ == '__main__':
+    #Error Log File
+    error_log_file = open("feedback-get-profile-timeline-mention.err", "a")
+    
+    #Twitter API tokens
+    ACCESS_TOKEN = keysandsecrets_json['IUNI_TWITTERAPI_ACCESS_TOKEN']
+    ACCESS_SECRET = keysandsecrets_json['IUNI_TWITTERAPI_ACCESS_SECRET']
+    CONSUMER_KEY = keysandsecrets_json['IUNI_TWITTERAPI_CONSUMER_KEY']
+    CONSUMER_SECRET = keysandsecrets_json['IUNI_TWITTERAPI_CONSUMER_SECRET']
+
+    #Accessing Twitter API using the tokens
+    oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+    #Twitter Search API Instance
+    twitter = Twitter(auth=oauth)
+
     #connecting to the database
     pgsqlconn = psycopg2.connect(host='', user='', password='', dbname='', port='')
     #cursor needed to execute db operations
@@ -73,8 +92,18 @@ if __name__ == '__main__':
     
     #for loop on result above and retrieve null_profiles from Twitter API
     for user_id in null_profiles:
-        populated_target_userids_and_profiles[user_id[0]] = "profile filler test"
-    
+        try:
+            #User Profile query (rate-limit = 300requests/15min)
+            populated_target_userids_and_profiles[user_id[0]] = twitter.users.lookup(user_id=user_id[0])
+            #Checking if the list is empty
+            if not populated_target_userids_and_profiles[user_id[0]]:
+                no_profile_found_dict = {}
+                no_profile_found_dict['no-profile-found'] = 'No profile was available at the time when this user was reported'
+                populated_target_userids_and_profiles[user_id[0]] = no_profile_found_dict
+        except:
+            error_log_file.write("USER PROFILE COULD NOT BE RETRIEVED, " + "UserId=" + str(user_id[0]) + ", Error=" + str(sys.exc_info()[0]) + "\n")
+            continue
+            
     #populate null_profiles
     populate_target_profiles_timelinetweets_mentiontweets("target_profile", populated_target_userids_and_profiles)
     
@@ -84,8 +113,18 @@ if __name__ == '__main__':
     
     #for loop on result above and retrieve target_timeline_tweets from Twitter API
     for user_id in null_targettimelinetweets:
-        populated_target_userids_and_targettimelinetweets[user_id[0]] = "timeline tweets filler text"
-    
+        try:
+            #Timeline Tweets query (rate-limit = 1500requests/15min)
+            populated_target_userids_and_targettimelinetweets[user_id[0]] = twitter.statuses.user_timeline(user_id=user_id[0], count=200)
+            #Checking if the list is empty
+            if not populated_target_userids_and_targettimelinetweets[user_id[0]]:
+                no_timeline_tweets_found_dict = {}
+                no_timeline_tweets_found_dict['no-timeline-tweets-found'] = 'There were no timeline tweets available at the time when this user was reported'
+                populated_target_userids_and_targettimelinetweets[user_id[0]] = no_timeline_tweets_found_dict
+        except:
+            error_log_file.write("USER TWEET TIMELINE COULD NOT BE RETRIEVED, " + "UserId=" + str(user_id[0]) + ", Error=" + str(sys.exc_info()[0]) + "\n")
+            continue
+            
     #populate timeline_tweets
     populate_target_profiles_timelinetweets_mentiontweets("target_timeline_tweets", populated_target_userids_and_targettimelinetweets)
         
@@ -95,11 +134,23 @@ if __name__ == '__main__':
     
     #for loop on result above and retrieve target_mention_tweets from Twitter API
     for user_id in null_targetmentiontweets:
-        populated_target_userids_and_targetmentiontweets[user_id[0]] = "mention tweets filler text"
-      
+        try:
+            #Mention Tweets query (rate-limit = 75requests/15min)
+            populated_target_userids_and_targetmentiontweets[user_id[0]] = twitter.statuses.mentions_timeline(user_id=user_id[0], count=200)
+            #Checking if list is empty
+            if not populated_target_userids_and_targetmentiontweets[user_id[0]]:
+                no_mention_tweets_found_dict = {}
+                no_mention_tweets_found_dict['no-mention-tweets-found'] = 'There were no mention tweets available at the time when this user was reported'
+                populated_target_userids_and_targetmentiontweets[user_id[0]] = no_mention_tweets_found_dict
+        except:
+            error_log_file.write("USER MENTION TIMELINE NOT BE RETRIEVED, " + "UserId=" + str(user_id[0]) + ", Error=" + str(sys.exc_info()[0]) + "\n")
+            continue
     #populate mention_tweets
     populate_target_profiles_timelinetweets_mentiontweets("target_mention_tweets", populated_target_userids_and_targetmentiontweets)
   
+    #closing error log file
+    error_log_file.close()
+    
     #closing access to database
     botometer_cursor.close()
     pgsqlconn.close()
