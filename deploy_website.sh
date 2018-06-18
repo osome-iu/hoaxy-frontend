b17cf79e431830b9886b09063177ff22d6febc5a
Ben Serrette <bserrett@iu.edu>
2018-06-18 12:28:43 -0400

    Readds the deployment script to deployment branch.

---
+++ deploy_website.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chgrp -R truthy frontend &&
+chmod -R a+r,g+rw frontend &&
+rsync -r frontend/ /home/data/www/hoaxy-website &&
+echo "Site is updated."
+echo "Don't forget to commit and push any changes made on the server."
