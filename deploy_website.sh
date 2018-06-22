fe83510b04fbbf1e3af0768c8a8b6829bd2c2fd3
Truthy <truthyatindiana@gmail.com>
2018-06-22 12:18:25 -0400

    Fixes deploy website script with wrong line endings.

--- deploy_website.sh
+++ deploy_website.sh
@@ -2,5 +2,6 @@
 chgrp -R truthy frontend &&
 chmod -R a+r,g+rw frontend &&
 rsync -r frontend/ /home/data/www/hoaxy-website &&
-echo "Site is updated."
+echo "Site is updated." &&
 echo "Don't forget to commit and push any changes made on the server."
+
