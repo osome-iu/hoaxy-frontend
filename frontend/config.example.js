12a40299eda6e86fba13f4e3be2394bfafb91d95
Marc McCarty <maruku115@gmail.com>
2019-09-20 10:39:07 -0400

    Updating mash->rap

--- frontend/config.example.js
+++ frontend/config.example.js
@@ -1,35 +1,35 @@
 configuration = function(){
 	var obj = {}
 
-	var mashape_key = "YOURKEY";
+	var rapidapi_key = "YOURKEY";
 
-	obj.articles_url = "https://api-hoaxy.p.mashape.com/articles";
+	obj.articles_url = "https://api-hoaxy.p.rapidapi.com/articles";
 	obj.articles_headers = {
-		"X-Mashape-Key": mashape_key,
+		"X-RapidAPI-Key": rapidapi_key,
 		"Accept": "application/json"
 	};
 
-	obj.timeline_url =  "https://api-hoaxy.p.mashape.com/timeline";
+	obj.timeline_url =  "https://api-hoaxy.p.rapidapi.com/timeline";
 	obj.timeline_headers = {
-		"X-Mashape-Key": mashape_key,
+		"X-RapidAPI-Key": rapidapi_key,
 		"Accept": "application/json"
 	};
 
-	obj.network_url = "https://api-hoaxy.p.mashape.com/network";
+	obj.network_url = "https://api-hoaxy.p.rapidapi.com/network";
 	obj.network_headers = {
-		"X-Mashape-Key": mashape_key,
+		"X-RapidAPI-Key": rapidapi_key,
 		"Accept": "application/json"
 	};
 
-	obj.top_articles_url = 'https://api-hoaxy.p.mashape.com/top-articles';
-	obj.top_articles_headers = { 'X-Mashape-Key': mashape_key};
+	obj.top_articles_url = 'https://api-hoaxy.p.rapidapi.com/top-articles';
+	obj.top_articles_headers = { 'X-RapidAPI-Key': rapidapi_key};
 
-	obj.top_users_url = 'https://api-hoaxy.p.mashape.com/top-users';
-	obj.top_users_headers = { 'X-Mashape-Key': mashape_key};
+	obj.top_users_url = 'https://api-hoaxy.p.rapidapi.com/top-users';
+	obj.top_users_headers = { 'X-RapidAPI-Key': rapidapi_key};
 
 	obj.botometer_url = "https://example.com";
 	obj.botometer_headers = {
-		"X-Mashape-Key": "--KEY--",
+		"X-RapidAPI-Key": "--KEY--",
 		"Accept": "application/json"
 	};
 
@@ -39,8 +39,6 @@ configuration = function(){
 	obj.twitter_key = "";
 
 	obj.botcache_chunk_sizes = [
-		// 50,
-		// 200,
 		2000
 	];
 
