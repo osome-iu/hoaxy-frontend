6f0c327286fcd2d6e1265e37ed178fb7ef8a549e
Marc McCarty <mem11@iu.edu>
2020-03-06 10:28:54 -0500

    Testing time format

--- frontend/static/js/timeline.js
+++ frontend/static/js/timeline.js
@@ -77,6 +77,21 @@ function HoaxyTimeline(settings){
 
 	chart.color([colors.edge_colors.claim, colors.edge_colors.fact_checking, "#00ff00"]); //color match with those of nodes
 
+
+	// var insertLinebreaks = function (d) {
+	// 	var el = d3.select(this);
+	// 	var words = d.split(' ');
+	// 	el.text('');
+	
+	// 	for (var i = 0; i < words.length; i++) {
+	// 		var tspan = el.append('tspan').text(words[i]);
+	// 		if (i > 0)
+	// 			tspan.attr('x', 0).attr('dy', '15');
+	// 	}
+	// };
+	
+	// svg.selectAll('g.x.axis g text').each(insertLinebreaks);
+
   /**
    * Redraw the timeline
    */
@@ -102,10 +117,10 @@ function HoaxyTimeline(settings){
   /**
    * Formats the date using d3.time
    * @param  {String} d The date to be formatted
-   * @return {String} The formatted time
+   * @return {String} The formatted timer
    */
 	function dateFormatter(d) {
-		return d3.time.format('%x')(new Date(d))
+		return d3.time.format('%x %H')(new Date(d))
 	}
 
   /**
@@ -136,7 +151,7 @@ function HoaxyTimeline(settings){
 
 			// Calculating New Claims for time stsep
 			var currentClaims = chartDataWithTweetRates[0].values[i].y;
-		  chartDataWithTweetRates[0].values[i].y =
+		    chartDataWithTweetRates[0].values[i].y =
 				currentClaims - previousTimestepClaims;
 			previousTimestepClaims = currentClaims;
 
