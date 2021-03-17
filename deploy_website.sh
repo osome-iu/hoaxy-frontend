505b6e4859374ed6718c30a35832f6faa09f7aba
Kaicheng(Kevin) Yang <yangkc@iu.edu>
2021-03-16 20:44:44 -0400

    Update deploy script
    Reflect the Lisa file structure

--- deploy_website.sh
+++ deploy_website.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chgrp -R truthy frontend &&
 chmod -R a+r,g+rw frontend &&
-rsync -r frontend/ /home/data/www/hoaxy-website &&
+rsync -r frontend/  /home/data/www/DocumentRoot/hoaxy.osome.iu.edu &&
 echo "Site is updated." &&
 echo "Don't forget to commit and push any changes made on the server."
-
