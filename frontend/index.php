fcece517ac009f3c9cefd95aca8680ee75c5a58e
Sarah Beverton <sbryant@iu.edu>
2021-05-21 18:50:10 -0400

    improves search button appearance

--- frontend/index.php
+++ frontend/index.php
@@ -40,8 +40,8 @@
         <div class="col-12 text-center">
           <div class="col-12 d-md-flex align-items-center">
             <div class="btn-group-vertical">
-              <div class="col-12 d-md-flex align-items-center pl-0">
-                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': twitterSearchSelected, 'text-light': twitterSearchSelected}">
+              <div class="col-12 text-center d-md-flex align-items-center pl-0">
+                <label class="d-flex align-items-center btn btn-primary search-btn mb-3" :class="{ 'active': twitterSearchSelected, 'text-light': twitterSearchSelected}">
                   <input id="searchByTwitter"
                     type="radio"
                     name="twitterOrHoaxy"
@@ -49,12 +49,12 @@
                     :checked="twitterSearchSelected"
                     @change="twitterSearch()"
                     >
-                  <span>Live Search</span>
+                    <span>Live Search</span>
                 </label>
                 <div class="ml-2 mb-3">Any Twitter content from the past 7 days</div>
               </div>
               <div class="col-12 text-center d-md-flex align-items-center pl-0">
-                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': hoaxySearchSelected, 'text-light': hoaxySearchSelected}">
+                <label class="d-flex align-items-center btn btn-primary search-btn mb-3" :class="{ 'active': hoaxySearchSelected, 'text-light': hoaxySearchSelected}">
                   <input id="searchByHoaxy"
                     type="radio"
                     name="twitterOrHoaxy"
@@ -62,18 +62,18 @@
                     :checked="hoaxySearchSelected"
                     @change="hoaxySearch()"
                     >
-                  <span>Article Search</span>
+                    <span>Article Search</span>
                 </label>
                 <div class="ml-2 mb-3">Twitter links to low-credibility and fact-checking sources</div>
               </div>
               <div class="col-12 text-center d-md-flex align-items-center pl-0">
-                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': importDataSelected, 'text-light': importDataSelected}">
+                <label class="d-flex align-items-center btn btn-primary search-btn mb-3" :class="{ 'active': importDataSelected, 'text-light': importDataSelected}">
                   <input type="radio"
-                      name="twitterOrHoaxy"
-                      autocomplete="off"
-                      :checked="importDataSelected"
-                      @change="importData()"
-                      >
+                    name="twitterOrHoaxy"
+                    autocomplete="off"
+                    :checked="importDataSelected"
+                    @change="importData()"
+                    >
                     <span>Import Data</span>
                   </label>
                 <div class="ml-2 mb-3">Upload a CSV or JSON file containing Tweet information</div>
