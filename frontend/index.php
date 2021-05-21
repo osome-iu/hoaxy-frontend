e19f3c1de57e2be790e4d520c9ca0dccfe43a7d9
Sarah Beverton <sbryant@iu.edu>
2021-05-21 11:28:06 -0400

    updates search buttons so the text and radio button look nice at different browser font settings

--- frontend/index.php
+++ frontend/index.php
@@ -41,7 +41,7 @@
           <div class="col-12 d-md-flex align-items-center">
             <div class="btn-group-vertical">
               <div class="col-12 d-md-flex align-items-center pl-0">
-                <label class="btn btn-primary search-btn mb-3" :class="{ 'active': twitterSearchSelected, 'text-light': twitterSearchSelected}">
+                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': twitterSearchSelected, 'text-light': twitterSearchSelected}">
                   <input id="searchByTwitter"
                     type="radio"
                     name="twitterOrHoaxy"
@@ -49,12 +49,12 @@
                     :checked="twitterSearchSelected"
                     @change="twitterSearch()"
                     >
-                  Live Search
+                  <span>Live Search</span>
                 </label>
                 <div class="ml-2 mb-3">Any Twitter content from the past 7 days</div>
               </div>
               <div class="col-12 text-center d-md-flex align-items-center pl-0">
-                <label class="btn btn-primary search-btn mb-3" :class="{ 'active': hoaxySearchSelected, 'text-light': hoaxySearchSelected}">
+                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': hoaxySearchSelected, 'text-light': hoaxySearchSelected}">
                   <input id="searchByHoaxy"
                     type="radio"
                     name="twitterOrHoaxy"
@@ -62,19 +62,19 @@
                     :checked="hoaxySearchSelected"
                     @change="hoaxySearch()"
                     >
-                  Article Search
+                  <span>Article Search</span>
                 </label>
                 <div class="ml-2 mb-3">Twitter links to low-credibility and fact-checking sources</div>
               </div>
               <div class="col-12 text-center d-md-flex align-items-center pl-0">
-                <label class="btn btn-primary search-btn mb-3" :class="{ 'active': importDataSelected, 'text-light': importDataSelected}">
+                <label class="flex-label btn btn-primary search-btn mb-3" :class="{ 'active': importDataSelected, 'text-light': importDataSelected}">
                   <input type="radio"
                       name="twitterOrHoaxy"
                       autocomplete="off"
                       :checked="importDataSelected"
                       @change="importData()"
                       >
-                    Import Data
+                    <span>Import Data</span>
                   </label>
                 <div class="ml-2 mb-3">Upload a CSV or JSON file containing Tweet information</div>
               </div>
